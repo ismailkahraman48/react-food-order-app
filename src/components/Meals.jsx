90d3b793f5b12e123fd4a901ced5fba9f9334520
ismail kahraman <ismailokahraman@gmail.com>
2023-12-23 14:50:13 +0300

    folder structure and components created

---
+++ src/components/Meals.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import MealItem from "./MealItem";
+
+function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch("http://localhost:3000/meals");
+      if (!response.ok) {
+      }
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem meal={meal} key={meal.id}/>
+      ))}
+    </ul>
+  );
+}
+
+export default Meals;
